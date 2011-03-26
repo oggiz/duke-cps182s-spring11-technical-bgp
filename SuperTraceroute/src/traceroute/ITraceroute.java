420c8324f8150220533d098014de39104f0720d4
hpkuarg@gmail.com <hpkuarg@gmail.com@5ff27d7f-96df-5ea2-5d5e-95f8271bb92d>
2011-03-26 23:54:25 +0000

    Initial commit.

---
+++ SuperTraceroute/src/traceroute/ITraceroute.java
@@ -0,0 +1,13 @@
+package traceroute;
+
+import java.net.InetAddress;
+
+/**
+ * 
+ */
+public interface ITraceroute
+{
+
+	public TracerouteItem traceroute(InetAddress address);
+
+}
---
+++ SuperTraceroute/src/traceroute/TracerouteItem.java
@@ -0,0 +1,14 @@
+package traceroute;
+
+import java.net.InetAddress;
+
+/**
+ * A class that represents one line of output from running traceroute.
+ */
+public class TracerouteItem
+{
+
+	private InetAddress address;
+	
+
+}
