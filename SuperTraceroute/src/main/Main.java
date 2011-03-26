11d2eab3f756e649ca9e5861a8e8458dbb6aa5f3
hpkuarg <hpkuarg@gmail.com>
2011-03-26 23:54:25 +0000

    Initial commit.

---
+++ SuperTraceroute/src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+/**
+ * The main program used to invoke the application.
+ */
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		
+	}
+
+}
---
+++ SuperTraceroute/src/traceroute/OSXTraceroute.java
@@ -0,0 +1,16 @@
+package traceroute;
+
+import java.net.InetAddress;
+
+/**
+ * Runs and parses traceroute in Mac OS X.
+ */
+public class OSXTraceroute implements ITraceroute
+{
+
+	public OSXTraceroute()
+	{
+		
+	}
+
+}
