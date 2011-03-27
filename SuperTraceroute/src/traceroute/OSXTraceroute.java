8dc7b3d49cb908300d6c75d2749eafb208dd81da
hpkuarg <hpkuarg@gmail.com>
2011-03-27 00:03:12 +0000

    Some updates.

--- SuperTraceroute/src/traceroute/OSXTraceroute.java
+++ SuperTraceroute/src/traceroute/OSXTraceroute.java
@@ -1,6 +1,10 @@
 package traceroute;
 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
+import traceroute.TracerouteItem;
 
 /**
  * Runs and parses traceroute in Mac OS X.
@@ -8,9 +12,42 @@ import java.net.InetAddress;
 public class OSXTraceroute implements ITraceroute
 {
 
+	private Runtime run;
+
 	public OSXTraceroute()
 	{
+		run = Runtime.getRuntime();
+	}
+
+	public TracerouteItem traceroute(InetAddress address)
+	{
+		Process pr = null;
+		String cmd = "traceroute " + address.toString();
 		
+		try
+		{
+			pr = run.exec(cmd);
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		BufferedReader buf = new BufferedReader(new InputStreamReader(
+				pr.getInputStream()));
+		
+		String line = "";
+		try
+		{
+			while((line = buf.readLine()) != null)
+			{
+				System.out.println(line);
+			}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 }
--- SuperTraceroute/src/traceroute/TracerouteItem.java
+++ SuperTraceroute/src/traceroute/TracerouteItem.java
@@ -3,12 +3,20 @@ package traceroute;
 import java.net.InetAddress;
 
 /**
- * A class that represents one line of output from running traceroute.
+ * A class that represents one line of output (that is, one hop) from running
+ * traceroute.
  */
 public class TracerouteItem
 {
 
+	/*
+	 * The IP address of this hop.
+	 */
 	private InetAddress address;
 	
+	/*
+	 * The hostname, if resolvable, of this hop.
+	 */
+	private String hostname;
 
 }
