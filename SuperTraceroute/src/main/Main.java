0064e93da188cbcf3a42b709292eb3a1e6d43a70
hpkuarg@gmail.com <hpkuarg@gmail.com@5ff27d7f-96df-5ea2-5d5e-95f8271bb92d>
2011-03-27 00:03:12 +0000

    Some updates.

--- SuperTraceroute/src/main/Main.java
+++ SuperTraceroute/src/main/Main.java
@@ -1,5 +1,9 @@
 package main;
 
+import traceroute.ITraceroute;
+import traceroute.OSXTraceroute;
+import traceroute.TracerouteItem;
+
 /**
  * The main program used to invoke the application.
  */
@@ -8,7 +12,9 @@ public class Main
 
 	public static void main(String[] args)
 	{
+		ITraceroute tr = new OSXTraceroute();
 		
+		TracerouteItem item = tr.traceroute();
 	}
 
 }
